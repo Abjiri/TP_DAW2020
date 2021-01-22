e3c38336888be53a5781aae6b107de7f8bfedb09
Válter <47797461+wurzy@users.noreply.github.com>
2021-01-22 22:46:28 +0000

    form

--- app-server/public/javascripts/showedit.js
+++ app-server/public/javascripts/showedit.js
@@ -20,13 +20,13 @@ function showEdit(){
 
 function pictureForm(id){
     var form = `
-    <form class="w3-container modal-content" action="/perfil/${id}/editar/imagem/" method="POST" style="border: 0; width: 100%" enctype="multipart/form-data" >
+    <form class="w3-container modal-content" action="/perfil/${id}/editar/imagem/" method="POST" style="border: 0; width: 100%">
     <div class="login_img">
         <span class="picture_close" title="Fechar" onclick="fechar()"> &times;</span>
     </div>
     <div "w3-row w3-margin-bottom">
         <div class="w3-col s3">
-            <label class="w3-text-teal"><b>Select file(s)</b></label>
+            <label class="w3-text-teal"><b>Selecionar Imagem: </b></label>
         </div>
         <div class="w3-col s9 w3-border">
             <input class="w3-input w3-border w3-light-grey" onchange="loadFile(event)" type="file" name="foto" accept="image/*" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
@@ -41,24 +41,45 @@ function pictureForm(id){
     $('#display').modal()
 }
 
-function editForm(id){
-    var form = `
-    <form class="w3-container modal-content" action="/perfil/${id}/editar/imagem/" method="POST" style="border: 0; width: 100%" enctype="multipart/form-data" >
-    <div class="login_img">
-        <span class="picture_close" title="Fechar" onclick="fechar()"> &times;</span>
+function equalSelect(f1,f2){
+    return f1 == f2 
+}
+
+function editForm(user){
+    var json = JSON.parse(user.replace(/\\/g, "/"))
+    var form = ` 
+    <form class="w3-container" action="/perfil/${json._id}/editar/" method="POST">
+    <div class="w3-row w3-margin-bottom">
+        <div class="w3-col s3">
+            <label class="w3-text-teal"><b>Nome:</b></label>
+        </div>
+        <div class="w3-col s9 w3-border">
+            <input class="w3-input w3-border w3-light-grey" type="text" name="nome" value="${json.nome}">
+        </div>
     </div>
     <div "w3-row w3-margin-bottom">
         <div class="w3-col s3">
-            <label class="w3-text-teal"><b>Select file(s)</b></label>
+            <label class="w3-text-teal"><b>Descrição:</b></label>
         </div>
         <div class="w3-col s9 w3-border">
-            <input class="w3-input w3-border w3-light-grey" onchange="loadFile(event)" type="file" name="foto" accept="image/*" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+            <input class="w3-input w3-border w3-light-grey" type="text" name="descricao" value="${json.descricao}">
         </div>
-        <img class="w3-margin-top w3-margin-bottom" id="output" style="width:100%"/>
     </div>
-    <input class="w3-btn w3-light-blue" type="submit" value="Confirmar" style="width: 100%"/>
-</form>
-    `
+    <div "w3-row w3-margin-bottom">
+    <div class="w3-col s3  w3-margin-top">
+        <label class="w3-text-teal"><b>Estatuto:</b></label>
+    </div>
+    <div class="w3-col s9 w3-border w3-margin-top">
+        <select name="cars" id="estatuto" style="width:100%">
+            <option value="estudante" ` + (json.estatuto.tipo == "estudante" ? `selected` : ``)  + `>Estudante</option>
+            <option value="docente" ` + (json.estatuto.tipo == "docente" ? `selected` : ``) + `>Docente</option>
+            <option value="trabalhador" ` + (json.estatuto.tipo == "trabalhador" ? `selected` : ``) + `>Trabalhador</option>
+        </select>
+    </div>
+    
+    </div>
+    <input class="w3-margin-top w3-btn w3-light-blue" type="submit" value="Confirmar" style="width: 100%"/>
+    </form>`
     $('#display2').empty()
     $('#display2').append(form)
     $('#display2').modal()
