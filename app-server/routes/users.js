8fc16f176e4bb6c90a960c390afb1ba6a61a532e
Hugo Cardoso <48107335+Abjiri@users.noreply.github.com>
2021-01-22 01:17:50 +0000

    Update users.js

--- app-server/routes/users.js
+++ app-server/routes/users.js
@@ -19,8 +19,7 @@ router.get('/logout', function(req, res) {
 router.post('/login', function(req, res) {
   axios.post('http://localhost:8000/users/login', req.body)
     .then(dados => {
-      console.log(dados.data)
-      if (dados.data.success) {
+      if (dados.data.token) {
         res.cookie('token', dados.data.token, {
           expires: new Date(Date.now() + '1d'),
           secure: false,
