a24c2b97c7b510594aecf587ece3be47f2550984
Hugo Cardoso <48107335+Abjiri@users.noreply.github.com>
2021-01-22 02:23:15 +0000

    registar sem rota

--- app-server/public/javascripts/navbar.js
+++ app-server/public/javascripts/navbar.js
@@ -5,17 +5,42 @@ $(document).ready(function()
         document.getElementById('login_modal').style.display='block';   
     });
 
+    $("#navbar_signup").click(function(event) {
+        event.preventDefault();
+        document.getElementById('signup_modal').style.display='block';   
+    });
+
     $(".login_close").click(function() {
         document.getElementById('login_modal').style.display='none';   
     });
 
-    $(".login_cancelbtn").click(function() {
+    $(".signup_close").click(function() {
+        document.getElementById('signup_modal').style.display='none';   
+    });
+
+    $("#login_cancelbtn").click(function() {
         document.getElementById('login_modal').style.display='none';   
     });
 
-    $(".login_reveal_pwd").click(function() {
-        var type = document.getElementById('login_password').getAttribute("type");
-        if (type == 'password') document.getElementById('login_password').setAttribute("type","text");
-        else document.getElementById('login_password').setAttribute("type","password");
+    $("#signup_cancelbtn").click(function() {
+        document.getElementById('signup_modal').style.display='none';   
+    });
+
+    $("#login_reveal_pwd").click(function() {
+        var pwd = document.getElementById('login_password');
+        if (pwd.getAttribute("type") == 'password') pwd.setAttribute("type","text");
+        else pwd.setAttribute("type","password");
+    });
+
+    $("#signup_reveal_pwd").click(function() {
+        var pwd = document.getElementById('signup_password');
+        if (pwd.getAttribute("type") == 'password') pwd.setAttribute("type","text");
+        else pwd.setAttribute("type","password");
+    });
+
+    $("#signup_reveal_pwd_again").click(function() {
+        var pwd = document.getElementById('signup_password_again');
+        if (pwd.getAttribute("type") == 'password') pwd.setAttribute("type","text");
+        else pwd.setAttribute("type","password");
     });
   });
--- app-server/routes/users.js
+++ app-server/routes/users.js
@@ -19,7 +19,39 @@ router.get('/logout', function(req, res) {
 router.post('/login', function(req, res) {
   axios.post('http://localhost:8000/users/login', req.body)
     .then(dados => {
-      console.log(dados.data)
+      if (dados.data.token) {
+        res.cookie('token', dados.data.token, {
+          expires: new Date(Date.now() + '1d'),
+          secure: false,
+          httpOnly: true
+        })
+
+        res.redirect('/recursos')
+      }
+      else res.render('index', {
+        auth: false,
+        invalidLoginField: dados.data.invalidInput,
+        email: req.body.email,
+        password: req.body.password,
+        error_msg: dados.data.error
+      })
+    })
+    .catch(error => res.render('error', {error}))
+})
+
+router.post('/signup', function(req, res) {
+  if (req.body.password != req.body.password_again) 
+    res.render('index', {
+      auth: false,
+      invalidSignupField: "password",
+      email: req.body.email,
+      pwd: req.body.password,
+      pwd_again: req.body.password_again,
+      error_msg: "As passwords não coincidem!"
+    })
+
+  axios.post('http://localhost:8000/users/signup', req.body)
+    .then(dados => {
       if (dados.data.success) {
         res.cookie('token', dados.data.token, {
           expires: new Date(Date.now() + '1d'),
@@ -31,9 +63,10 @@ router.post('/login', function(req, res) {
       }
       else res.render('index', {
         auth: false,
-        invalidInput: dados.data.invalidInput,
+        invalidSignupField: dados.data.invalidInput,
         email: req.body.email,
-        password: req.body.password,
+        pwd: req.body.password,
+        pwd_again: req.body.password_again,
         error_msg: dados.data.error
       })
     })
