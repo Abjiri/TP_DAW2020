7ef9e8a818324708fec1cb224b5f64ec1dc4bff0
Válter <47797461+wurzy@users.noreply.github.com>
2021-02-01 04:02:12 +0000

    posts and comments

--- api-server/controllers/publicacao.js
+++ api-server/controllers/publicacao.js
@@ -9,3 +9,23 @@ module.exports.listar = () => {
         .sort('-dataCriacao')
         .exec()
 }
+
+module.exports.consultar = id => {
+    return Publicacao
+        .findOne({_id: id})
+        .exec()
+}
+
+module.exports.adicionarComentario = (id, com) => {
+    return Publicacao
+        .findOneAndUpdate(
+            {_id: id},
+            {$push: {comments: com}},
+            {useFindAndModify: false, new: true}
+        )
+}
+
+module.exports.inserir = t => {
+    var novo = new Publicacao(t)
+    return novo.save()
+}
--- app-server/routes/post.js
+++ app-server/routes/post.js
@@ -4,16 +4,38 @@ var router = express.Router();
 var axios = require('axios');
 var func = require('./functions')
 
+
+router.get('/:id', function(req,res,next){
+    if (!req.cookies.token) res.redirect('/')
+    else {
+        axios.get('http://localhost:8001/publicacoes/' + req.params.id + '?token=' + req.cookies.token)
+            .then(dados => res.render('publicacao', {publicacao: dados.data}))
+            .catch(error => res.render('error', {error}))
+    }
+})
+
 router.post('/', function(req, res, next){
     if (!req.cookies.token) res.redirect('/')
     else {
         var token = func.unveilToken(req.cookies.token)
-        var dados = req.body
-        console.log(dados)
-        /*axios.post('http://localhost:8001/posts/' + req.params.id +'?token=' + req.cookies.token, fixed)
-            .then(dados => res.redirect("/perfil"))
+        req.body["id_autor"] = token._id
+        req.body["dataCriacao"] = new Date().toISOString().substr(0,16)
+        console.log(req.body)
+        axios.post('http://localhost:8001/publicacoes?token=' + req.cookies.token, req.body)
+            .then(dados => res.redirect("/publicacoes/"+dados.data._id))
             .catch(error => res.render('error', {error}))
-            */
+    }
+})
+
+router.post('/comentar/:id', function(req, res, next){
+    if (!req.cookies.token) res.redirect('/')
+    else {
+        var token = func.unveilToken(req.cookies.token)
+        req.body["id_autor"] = token._id
+        req.body["dataCriacao"] = new Date().toISOString().substr(0,16)
+        axios.post('http://localhost:8001/publicacoes/comentar/' + req.params.id + '?token=' + req.cookies.token, req.body)
+            .then(dados => res.redirect("/publicacoes/"+dados.data._id))
+            .catch(error => res.render('error', {error}))
     }
 })
 
