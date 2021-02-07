6a8bbb71e4697a55d3d063d2a73da2e48471740f
Válter <47797461+wurzy@users.noreply.github.com>
2021-02-07 19:21:18 +0000

    block user if blocked

--- auth-server/models/user.js
+++ auth-server/models/user.js
@@ -12,7 +12,8 @@ var userSchema = new mongoose.Schema({
     dataRegisto: {type: Date, default: Date.now},
     dataUltimoAcesso: {type: Date, default: Date.now},
     descricao: {type: String, default: ""},
-    foto: {type: String, default: ""}
+    foto: {type: String, default: ""},
+    bloqueado: {type: Boolean, required: true, default: false}
   });
 
 module.exports = mongoose.model('user', userSchema)
